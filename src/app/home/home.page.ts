5b4cae7fb1eb096832f2a343bc8b4136e81d5f03
aurelcamp <aureliencampistron@yahoo.fr>
2021-01-19 19:56:40 +0100

    'u'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+
+const { Camera } = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -8,5 +11,20 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  async openCamera() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      resultType: CameraResultType.Uri,
+      saveToGallery: true,
+    });
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    console.log(image);
+    var imageUrl = image.webPath;
+
+  }
 
 }
